887a77bd8a2c84cce5fe4c9567cb616361afad73
Softchair <24703877+Softchair@users.noreply.github.com>
2020-07-20 01:44:43 -0500

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -1,24 +1,35 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-
-	guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-	})
-    })
-})
-
-client.on('ready', () => {
-    var generalChannel = client.channels.get("387382140582625287")
-    generalChannel.send("Wowee this does a thing now")  
-})
+    console.log('Ready for action. Logged in as SoftchairBot');
+});
 
 
+client.on('message', message => {
+  if (!message.guild) return;
 
-bot_secret_token = "BOT TOKEN"
+  if (message.content === '-help') {
+    message.reply('Commands: -help -profilepic -repeat ping')
+  }
 
-client.login(bot_secret_token)
+  if(message.content.startsWith('-repeat')) {
+    //take sentence and makes it an array
+    let sentence = message.content.split(' ')
+    //.shift alters list remotes the first things in the list
+    sentence.shift();
+    sentence = sentence.join(' ');
+
+    message.channel.send(sentence);
+  }
+
+  if (message.content === '-profilepic') {
+    message.reply(message.author.displayAvatarURL());
+ }
+
+  if (message.content === 'ping') {
+    message.channel.send('pong')
+  }
+});
+
+client.login('NjI0MDIzNDgyMjE2MzQ5NzE4.XxSc-g.CT7fHslYiuWs3zeuzICXh-f6Ne8');
