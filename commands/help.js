566a850541fd9da33fb732e925dfa32574913741
Softchair <24703877+Softchair@users.noreply.github.com>
2020-11-12 17:15:20 -0600

    fixed help

--- commands/help.js
+++ commands/help.js
@@ -1,24 +1,22 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-  const helpEmbed = new Discord.MessageEmbed(
-    {
-  "embeds": [
-    {
-      "title": "Commands",
-      "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      "color": 15142928,
-      "fields": [
-        {
-          "name": "Commands - Description - Args",
-          "value": "-Help - This Command - None\n\n-Profilepic - Grabs your or some other users profile picture - Mention a user to get theirs\n\n-Repeat - Repeats your message, as the bot -- None\n\n-Ping - Used for testing the bots online and returns latency - None",
-          "inline": true
-        }
-      ],
-      "author": {
-        "name": "SoftchairBot"
-      }
-    }
-  ]
-});
-  message.author.send(helpEmbed);
+  const helpEmbed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Commands")
+    .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    .addFields({
+      name: "Commands",
+      value: "-Help\n\n-Profilepic\n\n-Repeat\n\n-Ping",
+      inline: true
+    }, {
+      name: "Description",
+      value: "Responds with this message\n\nSends users profile picture\n\nRepeats the message, as the bot\n\nTests bots onlineness, also latency",
+      inline: true
+    }, {
+      name: "Arguments",
+      value: "None\n\nMention a user\n\nNone\n\nNone",
+      inline: true
+    }, )
+    .setTimestamp()
+  message.channel.send(helpEmbed);
 }
