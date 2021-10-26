efbad6169e39ea3ca7022a316a86689bd6427a41
Softchair <24703877+Softchair@users.noreply.github.com>
2021-10-26 17:40:19 -0500

    adding apod

---
+++ commands/apod.js
@@ -0,0 +1,32 @@
+exports.run = (client, message, args) => {
+
+  //Set up the call request
+  var req = new XMLHttpRequest();
+  var url = "https://api.nasa.gov/planetary/apod?api_key=";
+
+  req.open("GET", url + nasaAPIkey);
+  req.send();
+
+  //Gets the data and adds it to vars
+  req.addEventListener("load", function() {
+    if (req.status == 200 && req.readyState == 4) {
+      var response = JSON.parse(req.responseText);
+      document.getElementById("title").textContent = response.title;
+      var date = document.getElementById("date").textContent = response.date;
+      var picture = document.getElementById("pic").src = response.hdurl;
+      var info = document.getElementById("explanation").textContent = response.explanation;
+    }
+  })
+
+  //Send its to the channel
+  message.channel.send(picture);
+  message.channel.send(date);
+  message.channel.send(info);
+
+}
+
+module.help = {
+  name: "APOD (Astronomy picture of the day)",
+  description: "Send the astrononmy picture of the day, and also information about it",
+  usage: "apod",
+}
