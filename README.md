cb6a7544c42e7fbeae9ac7e6911edbc4cfe6f5ab
Softchair <24703877+Softchair@users.noreply.github.com>
2021-10-26 20:13:18 -0500

    Created main and hid unneeded files

--- README.md
+++
@@ -1,7 +0,0 @@
-# discordbot
-
-Personal use discord bot with many features personalized for me/my discord server
-
-Use node mybot.js to launch the bot
-
-Make sure to have node.js installed aswell as discord.js
---
+++ src/main/java/botmain.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.AccountType;
+
+/**
+ * @author Camden
+ *
+ * Main for discord bot, more to be added soon
+ */
+public class botmain {
+    public static void main(String[] args) {
+
+    }
+}
