4655f8bdc8d732dce7de397d7c51eef569090d77
Softchair <24703877+Softchair@users.noreply.github.com>
2022-10-28 19:19:52 -0500

    no

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -34,28 +34,18 @@ public class Commands extends ListenerAdapter {
 
         System.out.println("Messaged received from " + event.getAuthor().getName() + ": " +event.getMessage().getContentDisplay());
 
-        if (msg.equals("!cf") || msg.equals("!coinflip")) { //Coinflip
-            Random ran = new Random();
-            int num = ran.nextInt(100);
-            if (num < 50) {
-                channel.sendMessage("Tails!").queue();
-            } else if (num > 50) {
-                channel.sendMessage("Heads!").queue();
-            } else {
-                channel.sendMessage("Perfect even! Reroll").queue();
-            }
-
-        } else if (msg.startsWith("!repeat")) {// Repeat command
-            String[] temp = msg.split(" ", 2);
-            channel.sendMessage(temp[1]).queue();
+     
             message.delete().queue();
+        }
 
-        } else if (msg.startsWith("!whoami")) { //Who am i command
+        private void whoAmI() {
             channel.sendMessage("You are " + author + "!").queue();
             channel.sendMessage("Your ID is " + author.getId()).queue();
             channel.sendMessage("Lets see if you're a bot: " + author.isBot()).queue();
-
-        } else if (msg.startsWith("!pfp") || msg.startsWith("!profilepic")) { //Profile pic command
+        } 
+        
+        
+        private void profilePic() {
             if (msg.startsWith("!pfp") && msg.length() > 4 || msg.startsWith("!profilepic") && msg.length() > 11) {
                 List<User> mention = message.getMentionedUsers();
                 for (int i = 0; i < mention.size(); i++) { //Can now mention multiple people and get all avatars
@@ -74,26 +64,28 @@ public class Commands extends ListenerAdapter {
                 }
                 channel.sendMessage(author.getAvatarUrl()).queue();
             }
-
-        } else if (msg.equals("!ping")) { //Ping command to check if online
+        } 
+        
+        private void onlineStatus() {
             channel.sendMessage("Pong! I'm online!").queue();
+        } 
 
-        } else if (msg.startsWith("!status")) { //Set status command
+        private void statusMessage() {
             if (author.getId().equals("286647094456877056")) {
                 String[] temp = msg.split(" ", 2);
                 bot.getPresence().setActivity(Activity.playing(temp[1]));
             } else {
                 channel.sendMessage("You are not the owner, this command doesn't work for you").queue();
             }
+        } 
 
-        } else if (msg.equals("!shutdown")) { //Shutdown command
+        private void shutdown() {
             if (author.getId().equals("286647094456877056")) {
                 channel.sendMessage("Shutting down, goodbye!").queue();
                 bot.shutdown();
             } else {
                 channel.sendMessage("You are not the owner, this command doesn't work for you").queue();
             }
-
         }
 
     }
